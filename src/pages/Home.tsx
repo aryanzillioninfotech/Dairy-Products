d48d7360ff6b03984f6b81e7f792ca6f5cc67fbb
aryanzillioninfotech <aryan.zillioninfotech@gmail.com>
2025-11-14 16:21:22 +0530

    those firstfour product which is in home page that is also done by MockAPI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../components/Hero";
-import { products } from "../data/products";
 import ProductCard from "../components/ProductCard";
-import "../assets/scss/home.scss"
+import "../assets/scss/home.scss";
+import axios from "axios";
+import type { Product } from "../data/products";
 
-const Home: React.FC = () => (
+const Home: React.FC = () => {
+  const [firstFour, setFirstFour] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchFirstFourData = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/products");
+        setFirstFour(res.data);
+      } catch (error) {
+        console.error("Error fetching first four products:", error);
+      }
+    };
+
+    fetchFirstFourData();
+  }, []);
+
+  console.log(firstFour);
+
+  return (
     <div className="home">
-        <Hero />
-        <div className="container">
-            <h2>Popular Picks</h2>
-            <div className="grid">
-                {products.slice(0, 4).map((p) => (
-                    <ProductCard key={p.id} product={p} />
-                ))}
-            </div>
+      <Hero />
+      <div className="container">
+        <h2>Popular Picks</h2>
+
+        <div className="grid">
+          {firstFour.slice(0, 4).map((p) => (
+            <ProductCard key={p.id} product={p} />
+          ))}
         </div>
+      </div>
     </div>
-);
+  );
+};
+
 export default Home;
